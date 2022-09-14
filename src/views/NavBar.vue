f141c3ee304e7acc9b260ff1e67fe209a0abd0b6
Lucas Mariano <lucas.svmariano@gmail.com>
2022-09-14 17:54:34 -0300

    feature de logout implementada

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -12,27 +12,9 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+      <v-btn @click="logout()">
+        <v-icon> mdi-arrow-left</v-icon> Deslogar
       </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-app-bar>
   </div>
 </template>
@@ -40,6 +22,12 @@
 <script>
 export default {
   name: "NavBar",
+  methods: {
+    logout() {
+      localStorage.removeItem("authUser");
+      this.$router.push({ name: "login" });
+    },
+  },
 };
 </script>
 
